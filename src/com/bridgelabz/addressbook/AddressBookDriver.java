1344b8254d276b493c239088007f10353ec5414a
Avani14 <trivediavani14@gmail.com>
2022-07-10 20:48:49 +0530

    Added the ability to update data.

--- src/com/bridgelabz/addressbook/AddressBookDriver.java
+++ src/com/bridgelabz/addressbook/AddressBookDriver.java
@@ -16,7 +16,7 @@ public class AddressBookDriver {
             System.out.println(" ---- MENU ----");
             System.out.println(" 1. Add Address Book\n 2. Add contacts\n 3. Display contacts" +
                     "\n 4. Edit contact\n 5. Delete Contact\n 6. Find Contacts with same state" +
-                    "\n 7. Find Contacts with same city \n 8.Sort the contacts by City State Or Zip.\n 9. Exit");
+                    "\n 7. Find Contacts with same city \n 8.Sort the contacts by City State Or Zip.\n 10. Exit");
             option = sc.next();
 
             switch (option) {
@@ -41,7 +41,48 @@ public class AddressBookDriver {
                     addressBookToDB.getDataByTimeStamp(start_time,end_time);
                     break;
                 case "4":
-                    AddressBookService.editContact();
+//                    AddressBookService.editContact();
+                    System.out.println("Enter the address book name");
+                    String name = sc.next();
+                    System.out.println("Enter the first name");
+                    String first_name = sc.next();
+                    System.out.println("Enter 1 to edit address\nEnter 2 to edit city\nEnter 3 to edit state\nEnter 4 to edit zip" +
+                            "\nEnter 5 to edit phone_number\nEnter 3 to edit email");
+                    int option2  = sc.nextInt();
+                    switch (option2){
+                        case 1:
+                            System.out.println("Enter the updated address");
+                            String address = sc.nextLine();
+                            addressBookToDB.updateData("address",address,name,first_name);
+                            break;
+                        case 2:
+                            System.out.println("Enter the updated city");
+                            String city = sc.next();
+                            addressBookToDB.updateData("city",city,name,first_name);
+                            break;
+                        case 3:
+                            System.out.println("Enter the updated state");
+                            String state = sc.next();
+                            addressBookToDB.updateData("state",state,name,first_name);
+                            break;
+                        case 4:
+                            System.out.println("Enter the updated zip");
+                            int zip = sc.nextInt();
+                            addressBookToDB.updateData("zip", String.valueOf(zip),name,first_name);
+                            break;
+                        case 5:
+                            System.out.println("Enter the phone number");
+                            long phone_number = sc.nextLong();
+                            addressBookToDB.updateData("phone_number", String.valueOf(phone_number),name,first_name);
+                            break;
+                        case 6:
+                            System.out.println("Enter the email address");
+                            String email = sc.next();
+                            addressBookToDB.updateData("email",email,name,first_name);
+                            break;
+                        default:
+                            System.out.println("Please enter valid option");
+                    }
                     break;
                 case "5":
                     AddressBookService.deleteContact();
@@ -57,10 +98,10 @@ public class AddressBookDriver {
                     AddressBookService.findSameCityContacts(city);
                     break;
                 case "8":
-                    int option2;
+                    int opt;
                     System.out.println("\t\nEnter 1: To sort by city:\nEnter 2: To sort by state:\nEnter 3: To sort by zip:");
-                    option2 = sc.nextInt();
-                    switch (option2){
+                    opt = sc.nextInt();
+                    switch (opt){
                         case 1 : AddressBookService.sortByCity();
                             break;
                         case 2 : AddressBookService.sortByCity();
--- src/com/bridgelabz/addressbook/databaseconnection/IAddressBookToDB.java
+++ src/com/bridgelabz/addressbook/databaseconnection/IAddressBookToDB.java
@@ -3,4 +3,5 @@ package com.bridgelabz.addressbook.databaseconnection;
 public interface IAddressBookToDB {
     void getData();
     void getDataByTimeStamp(String start_time,String end_time);
+    void updateData(String field,String setField,String address_book,String first_name);
 }
